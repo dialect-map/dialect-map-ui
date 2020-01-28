2d63ad5e93e39532e35c30bca8df3e8428228ec0
Sinclert Pérez <Sinclert@hotmail.com>
2020-01-28 13:12:07 -0500

    Simplify subdomains requests

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@
 
 
 /* Color tiles provider */
-export const colorTilesHost = 'paperscape.org/world/tiles';
+export const colorTilesHost = 'https://tile{s}.paperscape.org/world/tiles/{z}/{x}/{y}.png';
 export const colorTilesAttr = '<a href=https://github.com/paperscape>PaperScape</a>';
 
 /* Greyscale tiles provider */
-export const greyTilesHost = 'paperscape.org/world/tiles-hm';
+export const greyTilesHost = 'https://tile{s}.paperscape.org/world/tiles-hm/{z}/{x}/{y}.png';
 export const greyTilesAttr = '<a href=https://github.com/paperscape>PaperScape</a>';
--- src/pages/papers/controllers/PapersTilesLayer.js
+++ src/pages/papers/controllers/PapersTilesLayer.js
@@ -1,36 +1,23 @@
 /* encoding: utf-8 */
 
 import { GridLayer, withLeaflet } from "react-leaflet";
-import { TileLayer, } from "leaflet";
+import { TileLayer } from "leaflet";
 
 
 class PaperscapeTiles extends GridLayer {
 
 
-    getPaperscapeTileUrl(coords) {
-        console.log("Retrieving tile from Paperscape backend");
-
-        // PaperScape servers go from 1 to 4
-        let server = Math.round(Math.random() * 3) + 1;
-        let newZ = coords.z;
-        let newX = coords.x + 40;
-        let newY = coords.y + 40;
-
-        return `http://tile${server}.${this.options.url}/${newZ}/${newX}/${newY}.png`;
-    }
-
-
     // Function invoked by the GridLayer
     createLeafletElement(props) {
         let options = super.getOptions(props);
+        options.minZoom = 0;
+        options.maxZoom = 6;
         options.tileSize = 512;
-        options.minZoom = 1;
-        options.maxZoom = 10;
+        options.noWrap = true;
+        options.continuousWorld = true;
+        options.subdomains = ["1", "2", "3", "4"];
 
-        let layer = new TileLayer(props.url, options);
-        layer.getTileUrl = this.getPaperscapeTileUrl;
-
-        return layer
+        return new TileLayer(props.url, options);
     }
 
 
