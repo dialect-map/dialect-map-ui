d45176db7c2cd1760eed0cbf5a1c6f76a8ffa029
Sinclert Pérez <Sinclert@hotmail.com>
2020-02-03 11:59:22 -0500

    Impl. world-view position conversors

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@
 
 
 /* PaperScape map properties */
-export const mapBounds = [[-1750, 250], [-250, 1750]];
+export const mapBounds = [[-2000, 0], [0, 2000]];
 export const mapBoundsViscosity = 0.8;
 export const mapInitialCenter = [-1000, 1000];
 export const mapInitialZoom = 0;
--- src/pages/papers/components/tabs/Map.js
+++ src/pages/papers/components/tabs/Map.js
@@ -3,16 +3,17 @@
 import React, { Component } from "react";
 import * as config from "../../../../config";
 import PapersTilesLayer from "../../controllers/PapersTilesLayer";
-import { LayersControl, Map } from "react-leaflet";
+import { CircleMarker, LayersControl, Map } from "react-leaflet";
 import { CRS } from "leaflet";
 import "leaflet/dist/leaflet.css";
 
 
-const world_X_min = -73523;
-const world_X_max = 69456;
-
-const world_Y_min = -66067;
-const world_Y_max = 84802;
+/*
+ The complete specification can be obtained from:
+ https://tile1.paperscape.org/world/world_index.json
+ */
+const tilePixelSize = 512;
+const tilePixelsAtZoom0 = 37732;
 
 
 export default class MapCanvas extends Component {
@@ -20,9 +21,7 @@ export default class MapCanvas extends Component {
 
     constructor(props) {
         super(props);
-
-        // Set up at render() time
-        this.map = null;
+        this.map = null;  // Set up at render() time
     }
 
 
@@ -31,34 +30,30 @@ export default class MapCanvas extends Component {
     }
 
 
-    getPan() {
-        let centerLatLng = this.map.getCenter();
-        let centerCoords = this.map.latLngToLayerPoint(centerLatLng);
-        let originCoords = this.map.getPixelOrigin();
+    worldToViewScale() {
+        // Leaflet "Simple" CRS supposes a 1:1 ratio between tile pixels and world pixels at zoom 0.
+        // As it is not the case, scaling need to be performed
+        return tilePixelSize / tilePixelsAtZoom0;
+    }
 
+
+    worldToView(worldPaper) {
+        let scale = this.worldToViewScale();
+
+        // TODO: Why this offsets?
+        let Y_axis_offset = - 897.5;
+        let X_axis_offset = + 998.5;
+
+        // Leaflet considers [Y, X] not [X, Y]
         return [
-            centerCoords.x - originCoords.x,
-            centerCoords.y - originCoords.y
+            (-1 * worldPaper.y * scale) + Y_axis_offset,
+            (+1 * worldPaper.x * scale) + X_axis_offset
         ];
     }
 
 
-    worldToViewScale() {
-        let canvasBounds = this.map.getPixelBounds();
-        let canvasWidth = canvasBounds.max.x - canvasBounds.min.x;
-        let currentZoom = this.map.getZoom();
-        let worldWidth = world_X_max - world_X_min;
-
-        return (canvasWidth * currentZoom) / worldWidth;
-    }
-
-
-    viewToWorldScale() {
-        return 1 / this.worldToViewScale()
-    }
-
-
     render() {
+        const { papersList } = this.props;
 
         return (
             // The "ref" prop is necessary to obtain the created instance
@@ -75,21 +70,32 @@ export default class MapCanvas extends Component {
                 <LayersControl>
                     <LayersControl.BaseLayer
                         checked={false}
-                        name="Color">
+                        name="Discipline">
                         <PapersTilesLayer
                             url={config.colorTilesHost}
                             attribution={config.colorTilesAttr}
+                            tileSize={tilePixelSize}
                         />
                     </LayersControl.BaseLayer>
                     <LayersControl.BaseLayer
                         checked={true}
-                        name="Greyscale">
+                        name="Heatmap">
                         <PapersTilesLayer
                             url={config.greyTilesHost}
                             attribution={config.greyTilesAttr}
+                            tileSize={tilePixelSize}
                         />
                     </LayersControl.BaseLayer>
                 </LayersControl>
+
+                {papersList.map((paper, index) =>
+                    <CircleMarker
+                        key={index}
+                        center={this.worldToView(paper)}
+                        color={"red"}
+                        radius={4}>
+                    </CircleMarker>
+                )}
             </Map>
         );
     }
