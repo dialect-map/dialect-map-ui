c1a1ee843789936f2dd0a87a1c9ddebbe89ada27
Sinclert Pérez <Sinclert@hotmail.com>
2020-02-12 16:03:37 -0500

    Fix new-papers search

--- src/pages/papers/controllers/PaperSearch.js
+++ src/pages/papers/controllers/PaperSearch.js
@@ -12,12 +12,36 @@ export default class PaperSearchCtl  {
 
     static fetchPapersIDs(searchKey, searchValue) {
         let url = config.papersDataURL
-            + "?callback="
-            + "&" + searchKey + "=" + searchValue;
+            + this._buildRequestParams(searchKey, searchValue);
 
         return fetch(url, {})
             .then(resp => resp.text())
             .then(text => this._handlePaperSearchResp(text));
+    }
+
+
+    static _buildRequestParams(searchKey, searchValue) {
+        let params = "?callback=";
+
+        switch (searchKey) {
+            case "sau":
+                params += "&sau=" + searchValue;
+                break;
+            case "skw":
+                params += "&skw=" + searchValue;
+                break;
+            case "sti":
+                params += "&sti=" + searchValue;
+                break;
+            case "sca":
+                params += "&sca=" + searchValue + "&fd=1" + "&td=0";
+                break;
+            default:
+                params += "&skw=" + searchValue;
+                break;
+        }
+
+        return params;
     }
 
 
@@ -28,7 +52,7 @@ export default class PaperSearchCtl  {
             let body = this._prunePaperSearchResp(text);
             let json = JSON.parse(body);
             let data = json["r"];
-            paperIDs = data.map(paper => paper.id)
+            paperIDs = data.map(paper => paper.id);
         }
         catch(error) {
             console.log(error);
