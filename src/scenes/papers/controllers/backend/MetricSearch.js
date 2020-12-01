916c74cf8e17c21b4e52b644d2b720d73d613a25
Sinclert Pérez <Sinclert@hotmail.com>
2020-12-01 16:58:32 -0500

    Define MetricSearch controller

---
+++ src/scenes/papers/controllers/backend/MetricSearch.js
@@ -0,0 +1,34 @@
+/* encoding: utf-8 */
+
+import config from "../../../../config"
+import PaperJargonMetric from "../../models/PaperMetric";
+
+
+export default class MetricSearchCtl {
+
+
+    static fetchLatestMetrics(jargonID) {
+        let url = config.dialectMapURL
+            + "/paper/metrics/latest/"
+            + encodeURIComponent(jargonID);
+
+        return fetch(url, {})
+            .then(resp => resp.json())
+            .then(json => this._handleMetricSearchResp(json))
+            .catch(err => console.log(err));
+    }
+
+
+    static _handleMetricSearchResp(json) {
+        let metrics = [];
+
+        try {
+            metrics = json.map(metric => new PaperJargonMetric(metric));
+        }
+        catch(error) {
+            console.log(error);
+        }
+
+        return metrics;
+    }
+}
