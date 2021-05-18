cc16a035cf0909be6628715a7f492a70ad87e668
Sinclert Pérez <Sinclert@hotmail.com>
2021-05-18 15:01:12 -0400

    Clean-up React unused import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 /* encoding: utf-8 */
 
-import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter, Route } from "react-router-dom";
